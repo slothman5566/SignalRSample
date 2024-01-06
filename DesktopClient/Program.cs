b4585ccbb42c6ec32221bad6a98f2bf23a0cc9a3
slothman5566 <ccps960316@gmail.com>
2024-01-06 20:48:06 +0800

    ✨Client Add JWT Login

--- DesktopClient/Program.cs
+++ DesktopClient/Program.cs
@@ -1,17 +1,38 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
-using System;
-var url = "https://localhost:7255/sampleHub";
+
+var url = "https://localhost:7255";
+
+
+Console.WriteLine("Login:");
+var userId = Console.ReadLine();
+var client = new HttpClient();
 var connection = new HubConnectionBuilder()
-    .WithUrl(url)
+    .WithUrl($"{url}/sampleHub", options =>
+    {
+        options.AccessTokenProvider = async () =>
+        {
+            return await client.GetStringAsync($"{url}/jwt?user={userId}");
+        };
+
+    })
     .Build();
 
 await connection.StartAsync();
 
-connection.On<string>("ReceiveMessage", ( message) =>
+connection.On<string>("ReceiveMessage", (message) =>
 {
     Console.WriteLine($"{message}");
 });
 
+connection.On<string>("ReceiveMessage", (message) =>
+{
+    Console.WriteLine($"{message}");
+});
+
+connection.On<string>("TimedService", (message) =>
+{
+    Console.WriteLine($"TimedService :{message}");
+});
 
 
 Console.WriteLine("Press Ctrl+C to exit.");
