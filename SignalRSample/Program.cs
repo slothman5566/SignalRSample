423832d566a316e0d834bca51b7f9d36d5674de1
slothman5566 <ccps960316@gmail.com>
2024-01-08 22:27:21 +0800

    ✨Swagger Add Authorization

--- SignalRSample/Program.cs
+++ SignalRSample/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SignalRServer;
 using System.Security.Claims;
 using System.Text;
@@ -11,7 +13,37 @@ var secretKey = Guid.NewGuid().ToString();
 var jwtIssuer = "test";
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    // Authorization
+    options.AddSecurityDefinition("Bearer",
+    new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization"
+    });
+
+
+    options.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] {}
+            }
+        });
+});
 builder.Services.AddSignalR();
 
 builder.Services.AddSingleton<IJwtHandler>(new JwtHandler(secretKey, jwtIssuer, 30));
@@ -33,19 +65,24 @@ builder.Services.AddAuthentication(options =>
             ValidIssuer = jwtIssuer,
             ValidAudience = jwtIssuer,
             NameClaimType = ClaimTypes.NameIdentifier,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+            ClockSkew = TimeSpan.Zero
         };
         options.Events = new JwtBearerEvents
         {
             OnMessageReceived = context =>
             {
-                
+
                 var accessToken = context.Request.Query["access_token"];
 
                 var path = context.HttpContext.Request.Path;
                 if (path.StartsWithSegments("/sampleHub"))
                 {
                     context.Token = accessToken;
+                }
+                else if (context.Request.Headers.Authorization.Count > 0)
+                {
+                    context.Token = context.Request.Headers.Authorization.First();
                 }
                 return Task.CompletedTask;
             }
@@ -65,10 +102,11 @@ app.MapHub<SampleHub>("/sampleHub");
 app.UseAuthentication();
 app.UseAuthorization();
 
+
 app.MapPost("", async (string message, IHubContext<SampleHub> hub) =>
 {
     await hub.Clients.All.SendAsync("ReceiveMessage", message);
-});
+}).RequireAuthorization();
 
 app.MapGet("jwt", (string user, IJwtHandler jwtHandler) =>
 {
